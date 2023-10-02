d99e48b7a01fdfb5af3d9e3bbe943c94a0b2c4c5
Harsh Bhardwaj <mailharsh0218@gmail.com>
2023-10-02 18:26:20 +0530

    Homepage_Almost_Done

--- server.js
+++ server.js
@@ -5,14 +5,18 @@ const ejs=require('ejs')
 const expresLayout=require('express-ejs-layouts')
 const { setEngine } = require('crypto')
 const PORT= process.env.PORT || 3000
+
+
 app.get('/', (req,res)=>{
     res.render('home')
 })
+
+app.use(express.static('public'))
 
 //set template engine
 app.use(expresLayout)
 app.set('views', path.join(__dirname,'/resources/views'))
 app.set('view engine','ejs')
 app.listen(PORT, () =>{
-    console.log(`Harsh is the worst coder ever  ${PORT}`)
+    console.log(`Harsh is the done coder ever  ${PORT}`)
 })
